1e22baba0df4a45abdf688f3144e0ea923a29e91
Pavel Komarov <pvlkmrv@gmail.com>
2020-01-16 22:40:56 -0500

    updates to the project files to include my augmenations and work

--- Projects/3_Adversarial Search/isolation/__init__.py
+++ Projects/3_Adversarial Search/isolation/__init__.py
@@ -1,6 +1,5 @@
-
 ###############################################################################
-#                          DO NOT MODIFY THIS FILE                            #
+#						  DO NOT MODIFY THIS FILE							#
 ###############################################################################
 import inspect
 import logging
@@ -43,154 +42,152 @@ Loser: {}
 """
 
 class Status(Enum):
-    NORMAL = 0
-    EXCEPTION = 1
-    TIMEOUT = 2
-    INVALID_MOVE = 3
-    GAME_OVER = 4
+	NORMAL = 0
+	EXCEPTION = 1
+	TIMEOUT = 2
+	INVALID_MOVE = 3
+	GAME_OVER = 4
 
 
 class StopSearch(Exception): pass  # Exception class used to halt search
 
 
 class TimedQueue:
-    """Modified queue class to block .put() after a time limit expires,
-    and to include both a context object & action choice in the queue.
-    """
-    def __init__(self, receiver, sender, time_limit):
-        self.__sender = sender
-        self.__receiver = receiver
-        self.__time_limit = time_limit / 1000
-        self.__stop_time = None
-        self.agent = None
+	"""Modified queue class to block .put() after a time limit expires,
+	and to include both a context object & action choice in the queue.
+	"""
+	def __init__(self, receiver, sender, time_limit):
+		self.__sender = sender
+		self.__receiver = receiver
+		self.__time_limit = time_limit / 1000
+		self.__stop_time = None
+		self.agent = None
 
-    def start_timer(self):
-        self.__stop_time = self.__time_limit + time.perf_counter()
+	def start_timer(self):
+		self.__stop_time = self.__time_limit + time.perf_counter()
 
-    def put(self, item, block=True, timeout=None):
-        if self.__stop_time and time.perf_counter() > self.__stop_time:
-            raise StopSearch
-        if self.__receiver.poll():
-            self.__receiver.recv()
-        self.__sender.send((getattr(self.agent, "context", None), item))
+	def put(self, item):
+		if self.__stop_time and time.perf_counter() > self.__stop_time:
+			raise StopSearch
+		if self.__receiver.poll():
+			self.__receiver.recv()
+		self.__sender.send((getattr(self.agent, "context", None), item))
 
-    def put_nowait(self, item):
-        self.put(item, block=False)
+	def put_nowait(self, item):
+		self.put(item)
 
-    def get(self, block=True, timeout=None):
-        return self.__receiver.recv()
+	def get(self):
+		return self.__receiver.recv()
 
-    def get_nowait(self):
-        return self.get(block=False)
-
-    def qsize(self): return int(self.__receiver.poll())
-    def empty(self): return ~self.__receiver.poll()
-    def full(self): return self.__receiver.poll()
+	def get_nowait(self):
+		return self.get()
 
 
 def play(args): return _play(*args)  # multithreading ThreadPool.map doesn't expand args
 
 
 def _play(agents, game_state, time_limit, match_id, debug=False):
-    """ Run a match between two agents by alternately soliciting them to
-    select a move and applying it to advance the game state.
+	""" Run a match between two agents by alternately soliciting them to
+	select a move and applying it to advance the game state.
 
-    Parameters
-    ----------
-    agents : tuple
-        agents[i] is an instance of isolation.Agent class (namedtuple)
+	Parameters
+	----------
+	agents : tuple
+		agents[i] is an instance of isolation.Agent class (namedtuple)
 
-    game_state: Isolation
-        an instance of Isolation.Isolation in the initial game state;
-        assumes that agents[game_state.ply_count % 2] is the active
-        player in the initial state
+	game_state: Isolation
+		an instance of Isolation.Isolation in the initial game state;
+		assumes that agents[game_state.ply_count % 2] is the active
+		player in the initial state
 
-    time_limit : numeric
-        The maximum number of milliseconds to allow before timeout during
-        each turn (see notes)
+	time_limit : numeric
+		The maximum number of milliseconds to allow before timeout during
+		each turn (see notes)
 
-    Returns
-    -------
-    (agent, list<[(int, int),]>, Status)
-        Return multiple including the winning agent, the actions that
-        were applied to the initial state, a status code describing the
-        reason the game ended, and any error information
-    """
-    initial_state = game_state
-    game_history = []
-    winner = None
-    status = Status.NORMAL
-    players = [a.agent_class(player_id=i) for i, a in enumerate(agents)]
-    logger.info(GAME_INFO.format(initial_state, *agents))
-    while not game_state.terminal_test():
-        active_idx = game_state.player()
+	Returns
+	-------
+	(agent, list<[(int, int),]>, Status)
+		Return multiple including the winning agent, the actions that
+		were applied to the initial state, a status code describing the
+		reason the game ended, and any error information
+	"""
+	initial_state = game_state
+	game_history = []
+	winner = None
+	status = Status.NORMAL
+	players = [a.agent_class(player_id=i) for i, a in enumerate(agents)]
+	logger.info(GAME_INFO.format(initial_state, *agents))
+	while not game_state.terminal_test():
+		active_idx = game_state.player()
 
-        # any problems during get_action means the active player loses
-        winner, loser = agents[1 - active_idx], agents[active_idx]
+		# any problems during get_action means the active player loses
+		winner, loser = agents[1 - active_idx], agents[active_idx]
 
-        try:
-            action = fork_get_action(game_state, players[active_idx], time_limit, debug)
-        except Empty:
-            status = Status.TIMEOUT
-            logger.warn(textwrap.dedent("""\
-                The queue was empty after get_action() was called. This means that either
-                the queue.put() method was not called by the get_action() method, or that
-                the queue was empty after the procedure was killed due to timeout {} seconds
-                after the move time limit of {} milliseconds had expired.
-                """.format(players[active_idx], PROCESS_TIMEOUT, time_limit)).replace("\n", " "))
-            break
-        except Exception as err:
-            status = Status.EXCEPTION
-            logger.error(ERR_INFO.format(
-                err, initial_state, agents[0], agents[1], game_state, game_history
-            ))
-            break
+		try:
+			action = fork_get_action(game_state, players[active_idx], time_limit, debug)
+		except Empty:
+			status = Status.TIMEOUT
+			logger.warn(textwrap.dedent("""\
+				The queue was empty after get_action() was called. This means that either
+				the queue.put() method was not called by the get_action() method, or that
+				the queue was empty after the procedure was killed due to timeout {} seconds
+				after the move time limit of {} milliseconds had expired.
+				""".format(players[active_idx], PROCESS_TIMEOUT, time_limit)).replace("\n", " "))
+			break
+		except Exception as err:
+			status = Status.EXCEPTION
+			logger.error(ERR_INFO.format(
+				err, initial_state, agents[0], agents[1], game_state, game_history
+			))
+			break
 
-        if action not in game_state.actions():
-            status = Status.INVALID_MOVE
-            break
+		if action not in game_state.actions():
+			status = Status.INVALID_MOVE
+			break
 
-        game_state = game_state.result(action)
-        game_history.append(action)
-    else:
-        status = Status.GAME_OVER
-        if game_state.utility(active_idx) > 0:
-            winner, loser = loser, winner  # swap winner/loser if active player won
+		game_state = game_state.result(action)
+		game_history.append(action)
+	else:
+		status = Status.GAME_OVER
+		if game_state.utility(active_idx) > 0:
+			winner, loser = loser, winner  # swap winner/loser if active player won
 
-    logger.info(RESULT_INFO.format(status, game_state, game_history, winner, loser))
-    return winner, game_history, match_id
+	logger.info(RESULT_INFO.format(status, game_state, game_history, winner, loser))
+	if players[0].__class__.__name__ != 'CustomPlayer': players.reverse() # so CustomPlayer is always first
+	return winner, game_history, match_id, players
 
 
 def fork_get_action(game_state, active_player, time_limit, debug=False):
-    receiver, sender = Pipe()
-    action_queue = TimedQueue(receiver, sender, time_limit)
-    if debug:  # run the search in the main process and thread
-        from copy import deepcopy
-        active_player.queue = None
-        active_player = deepcopy(active_player)
-        active_player.queue = action_queue
-        _request_action(active_player, action_queue, game_state)
-        time.sleep(time_limit / 1000)
-    else:  # spawn a new process to run the search function
-        try:
-            p = Process(target=_request_action, args=(active_player, action_queue, game_state))
-            p.start()
-            p.join(timeout=PROCESS_TIMEOUT + time_limit / 1000)
-        finally:
-            if p and p.is_alive(): p.terminate()
-    new_context, action = action_queue.get_nowait()  # raises Empty if agent did not respond
-    active_player.context = new_context
-    return action
+	receiver, sender = Pipe()
+	action_queue = TimedQueue(receiver, sender, time_limit)
+	if debug:  # run the search in the main process and thread
+		from copy import deepcopy
+		active_player.queue = None
+		active_player = deepcopy(active_player)
+		active_player.queue = action_queue
+		_request_action(active_player, action_queue, game_state)
+		time.sleep(time_limit / 1000)
+	else:  # spawn a new process to run the search function
+		try:
+			p = Process(target=_request_action, args=(active_player, action_queue, game_state))
+			p.start()
+			p.join(timeout=PROCESS_TIMEOUT + time_limit / 1000)
+		finally:
+			if p and p.is_alive(): p.terminate()
+	new_context, action = action_queue.get_nowait()  # raises Empty if agent did not respond
+	active_player.context = new_context
+	return action
 
 
 def _request_action(agent, queue, game_state):
-    """ Augment agent instances with a countdown timer on every method before
-    calling the get_action() method and catch countdown timer exceptions.
-    """
-    agent.queue = queue
-    queue.agent = agent
-    try:
-        queue.start_timer()
-        agent.get_action(game_state)
-    except StopSearch:
-        pass
+	""" Augment agent instances with a countdown timer on every method before
+	calling the get_action() method and catch countdown timer exceptions.
+	"""
+	agent.queue = queue
+	queue.agent = agent
+	try:
+		queue.start_timer()
+		agent.get_action(game_state)
+	except StopSearch:
+		pass
+
--- Projects/3_Adversarial Search/my_custom_player.py
+++ Projects/3_Adversarial Search/my_custom_player.py
@@ -1,46 +1,153 @@
 
 from sample_players import DataPlayer
+import random
+from itertools import count
+from isolation import DebugState
 
 
 class CustomPlayer(DataPlayer):
-    """ Implement your own agent to play knight's Isolation
+	""" Implement your own agent to play knight's Isolation
 
-    The get_action() method is the only required method for this project.
-    You can modify the interface for get_action by adding named parameters
-    with default values, but the function MUST remain compatible with the
-    default interface.
+	The get_action() method is the only required method for this project.
+	You can modify the interface for get_action by adding named parameters
+	with default values, but the function MUST remain compatible with the
+	default interface.
 
-    **********************************************************************
-    NOTES:
-    - The test cases will NOT be run on a machine with GPU access, nor be
-      suitable for using any other machine learning techniques.
+	**********************************************************************
+	NOTES:
+	- The test cases will NOT be run on a machine with GPU access, nor be
+	  suitable for using any other machine learning techniques.
 
-    - You can pass state forward to your agent on the next turn by assigning
-      any pickleable object to the self.context attribute.
-    **********************************************************************
-    """
-    def get_action(self, state):
-        """ Employ an adversarial search technique to choose an action
-        available in the current state calls self.queue.put(ACTION) at least
+	- You can pass state forward to your agent on the next turn by assigning
+	  any pickleable object to the self.context attribute.
+	**********************************************************************
+	"""
+	def get_action(self, state):
+		""" Employ an adversarial search technique to choose an action
+		available in the current state.
 
-        This method must call self.queue.put(ACTION) at least once, and may
-        call it as many times as you want; the caller will be responsible
-        for cutting off the function after the search time limit has expired.
+		This method must call self.queue.put(ACTION) at least once, and may
+		call it as many times as you want; the caller will be responsible
+		for cutting off the function after the search time limit has expired.
 
-        See RandomPlayer and GreedyPlayer in sample_players for more examples.
+		See RandomPlayer and GreedyPlayer in sample_players for more examples.
 
-        **********************************************************************
-        NOTE: 
-        - The caller is responsible for cutting off search, so calling
-          get_action() from your own code will create an infinite loop!
-          Refer to (and use!) the Isolation.play() function to run games.
-        **********************************************************************
-        """
-        # TODO: Replace the example implementation below with your own search
-        #       method by combining techniques from lecture
-        #
-        # EXAMPLE: choose a random move without any search--this function MUST
-        #          call self.queue.put(ACTION) at least once before time expires
-        #          (the timer is automatically managed for you)
-        import random
-        self.queue.put(random.choice(state.actions()))
+		**********************************************************************
+		NOTE: 
+		- The caller is responsible for cutting off search, so calling
+		  get_action() from your own code will create an infinite loop!
+		  Refer to (and use!) the Isolation.play() function to run games.
+		**********************************************************************
+		"""
+		# This was tricky to figure out because queue isn't initualized here, so I had to go grep through the project
+		# to find it's a TimedQueue from __init__. A TimedQueue takes a sender and reciever from a multiprocessing
+		# Pipe() and is only initialized in fork_get_action. So what's going on here is fork_get_action calls
+		# _request_action, either in the same process or in another one, and then waits 150ms for it to run (or until
+		# the blocking process finishes). Meanwhile, _request_action calls start_timer(), which sets up the TimedQueue
+		# to throw a StopSearch exception 150ms later, inside a try block along with get_action, which presumably then
+		# repeatedly calls put() on the TimedQueue. put() calls poll() on the reciever and follows it with recv() if
+		# there's anything there, which means it self-clears before send()ing new information through the Pipe.
+		# _request_action catches the exception when the timer expires and exits gracefully. Back in the caller,
+		# fork_get_action, the TimedQueue is used to recieve the last thing sent through the Pipe, which is the most
+		# recent action. So: Iterative deepening can be accomplished here without doing anything but calling put() over
+		# and over with better answers. Too-deep depths just won't get tried, because this function will be forced to
+		# stop execution when the time runs out. Mystery solved.
+		if self.context is None: self.context = {'c_nodes': 0, 'c_layers': 0}
+
+		if state.ply_count < 2:
+			self.queue.put(random.choice(state.actions()))
+		else:
+			for depth in count(1):
+				pre_nodes = self.context['c_nodes']
+
+				self.queue.put(self.alpha_beta_search(state, depth, self.moves_diff_heuristic))
+
+				self.context['c_layers'] += depth
+				if self.context['c_nodes'] - pre_nodes == depth: return #finish early, because we ran out of nodes
+				
+
+	# Straight outta the lecture coding example
+	def alpha_beta_search(self, state, depth, heuristic):
+		""" Return the move along a branch of the game tree that
+		has the best possible value.  A move is a pair of coordinates
+		in (column, row) order corresponding to a legal move for
+		the searching player.
+		
+		You can ignore the special case of calling this function
+		from a terminal state.
+		"""
+		# helper function definitions
+		def min_value(state, depth, alpha, beta):
+			""" Return the value for a win (+1) if the game is over,
+			otherwise return the minimum value over all legal childz
+			nodes.
+			"""
+			self.context['c_nodes'] += 1
+
+			if state.terminal_test():
+				return state.utility(self.player_id)
+
+			if depth <= 0: return heuristic(state)
+			
+			v = float("inf")
+			for a in state.actions():
+				v = min(v, max_value(state.result(a), depth-1, alpha, beta))
+				if v <= alpha: return v
+				beta = min(beta, v)
+			return v
+
+		def max_value(state, depth, alpha, beta):
+			""" Return the value for a loss (-1) if the game is over,
+			otherwise return the maximum value over all legal child
+			nodes.
+			"""
+			self.context['c_nodes'] += 1
+
+			if state.terminal_test():
+				return state.utility(self.player_id)
+			
+			if depth <= 0: return heuristic(state)
+
+			v = float("-inf")
+			for a in state.actions():
+				v = max(v, min_value(state.result(a), depth-1, alpha, beta))
+				if v >= beta: return v
+				alpha = max(alpha, v)
+			return v
+
+		# kick it off
+		self.context['c_nodes'] += 1
+		alpha = float("-inf")
+		beta = float("inf")
+		best_score = float("-inf")
+		best_move = None
+		for a in state.actions():
+			v = min_value(state.result(a), depth-1, alpha, beta)
+			alpha = max(alpha, v) # this line disallows calling with just max(key=lambda)
+			if v > best_score:
+				best_score = v
+				best_move = a
+		return best_move
+
+	def moves_diff_heuristic(self, state):
+		return len(state.liberties(state.locs[self.player_id])) - \
+				len(state.liberties(state.locs[1-self.player_id]))	
+
+	# stay near the opponent, blocking his liberties if possible
+	# pure aggression
+	def chase_opponent_heuristic(self, state):
+		ind1 = state.locs[self.player_id]
+		x1, y1 = (ind1 % (11 + 2), ind1 // (11 + 2))
+		ind2 = state.locs[1-self.player_id]
+		x2, y2 = (ind2 % (11 + 2), ind2 // (11 + 2))
+		print(x1, y1)
+		print(DebugState.ind2xy(ind1))
+		for s in state.liberties(ind1):
+			print(DebugState.ind2xy(s))
+
+		print(x2, y2)
+		print(DebugState.ind2xy(ind2))
+		for s in state.liberties(ind2):
+			print(DebugState.ind2xy(s))
+		
+		return -(x1-x2)**2 - (y1-y2)**2 - len(state.liberties(ind2))
